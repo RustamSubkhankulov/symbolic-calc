5236b63a86dcd4c8713b1d0360c2d7a331abe0c4
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-09-17 12:21:51 +0300

    inc/lexer.h: introduced token structures for each class.

--- inc/lexer.h
+++ inc/lexer.h
@@ -18,6 +18,16 @@ typedef enum
 } token_class_t;
 
 /**
+ * @brief Base token struct. 
+ */
+typedef struct
+{
+  token_class_t class;
+} token_t;
+
+//---------------------------------------------
+
+/**
  * @brief Subtype of the operator tokens.
  */
 typedef enum
@@ -27,10 +37,11 @@ typedef enum
 } oper_subclass_t;
 
 /**
- * @brief Enumerated type, describing possible binary operations.
+ * @brief Enumerated type, describing possible binary and unary operators.
  */
 typedef enum
 {
+  /* Binary operators. */
   ADD, /* '+' */
   SUB, /* '-' */
   MUL, /* '*' */
@@ -38,20 +49,26 @@ typedef enum
   AND, /* '&' */
   OR, /* '|' */
   XOR, /* '^' */
-} binary_oper_t;
+
+  /* Unary operators. */
+  NEG, /* '~' */
+} oper_val_t;
 
 /**
- * @brief Enumerated type, describing possible unary operations.
+ * @brief Operator token struct.
  */
-typedef enum
+typedef struct
 {
-  NEG, /* '~' */
-} unary_oper_t;
+  token_t base;
+  oper_subclass_t subclass;
+  oper_val_t val;
+} oper_token_t;
+
+//---------------------------------------------
 
 /**
  * @brief Enumerated type, describing supported standard functions.
  */
-
 typedef enum
 {
   SIN, /* Sine. */
@@ -61,16 +78,53 @@ typedef enum
   SQRT, /* Square root. */
 } std_func_t;
 
+/**
+ * @brief Standard function token struct.
+ */
+typedef struct
+{
+  token_t base;
+  std_func_t val;
+} std_func_token_t;
+
+//---------------------------------------------
+
+/**
+ * @brief Enumerated type, describing supported standard constants.
+ */
 typedef enum
 {
   PI, /* Pi. */
   EXP, /* Euler's number. */
 } std_const_t;
 
+/**
+ * @brief Standard constant token struct.
+ */
+typedef struct
+{
+  token_t base;
+  std_const_t val;
+} std_const_token_t;
+
+//---------------------------------------------
+
+/**
+ * @brief Enumerated type, describing punctuation symbols.
+ */
 typedef enum
 {
   OP_BR, /* Opening bracket. */
   CL_BR, /* Closing bracket. */
 } punct_symb_t;
+
+/**
+ * @brief Punctuation symbol token struct.
+ */
+typedef struct
+{
+  token_t base;
+  punct_symb_t val;
+} punct_symb_token_t;
 
 #endif /* LEXER_H */
